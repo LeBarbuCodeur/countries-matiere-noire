88c00154791e3c23c95bbc2c5385f6e765aca97d
spubert <spubert@ideolys.com>
2021-07-16 08:24:04 +0200

    Update main app title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>European Countries</title>
+    <title>Countries</title>
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
     <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" />
   </head>
